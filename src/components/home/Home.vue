134db9fc569bb3a74e3b0270dded3228bda48a7c
hahachena <836118643@qq.com>
2020-11-01 16:58:24 +0800

    location

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icon></home-icon>
+    <home-location></home-location>
   </div>
 </template>
 
@@ -10,11 +11,13 @@
 import HomeHeader from "./page/Header";
 import HomeSwiper from "./page/Swiper";
 import HomeIcon from "./page/icon";
+import HomeLocation from "./page/location";
 export default {
   components: {
     HomeHeader,
     HomeSwiper,
     HomeIcon,
+    HomeLocation,
   }
 };
 </script>
---
+++ src/components/home/page/location.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="location border-top">
+    <div class="location-item border-right">
+      <span class="iconfont">&#xe63f;</span>
+      定位失败
+    </div>
+    <div class="location-item">
+      <span class="iconfont">&#xe6f2;</span>
+      游玩
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.location {
+  display: flex;
+  position: relative;
+}
+
+.location .location-item {
+  flex: 1;
+  font-size: 0.28rem;
+  text-align: center;
+  line-height: 1rem;
+}
+</style>
