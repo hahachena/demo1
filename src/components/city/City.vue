6b02a54c969d78e8c0185566b0ad0e92166c3033
hahachena <836118643@qq.com>
2020-11-01 21:58:31 +0800

    hot

--- src/components/city/City.vue
+++ src/components/city/City.vue
@@ -1,18 +1,27 @@
 <template>
-    <div>
+    <div class="city">
        <city-header></city-header>
+       <city-hot></city-hot>
+       <city-sort></city-sort>
     </div>
 </template>
 
 <script>
 import CityHeader from './page/Header'
+import CityHot from './page/Hot'
+import CitySort from './page/Sort'
 export default {
     components:{
         CityHeader,
+        CityHot,
+        CitySort,
     }
 }
 </script>
 
 <style scoped>
+.city{
+    background: #f5f5f5;
+}
 
 </style>
---
+++ src/components/city/page/Hot.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hot">
+    <div class="hot-title">热门城市</div>
+
+    <ul class="hot-list">
+      <li class="hot-item">北京</li>
+      <li class="hot-item">北京</li>
+      <li class="hot-item">北京</li>
+      <li class="hot-item">北京</li>
+      <li class="hot-item">北京</li>
+      <li class="hot-item">北京</li>
+      <li class="hot-item">北京</li>
+      <li class="hot-item">北京</li>
+      <li class="hot-item">北京</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.hot-title {
+  font-size: 0.24rem;
+  color: #212121;
+  padding: 0.2rem .3rem;
+}
+
+.hot-list {
+  background: #fff;
+  color: #212121;
+  font-size: 0.28rem;
+  overflow: hidden;
+  position: relative;
+}
+
+.hot-list::before {
+  content: "";
+  position: absolute;
+  height: 100%;
+  width: 33.3333333333333333333333333333333%;
+  left: 33.3333333333333333333333333333333%;
+  border-left: 0.02rem solid #ddd;
+  border-right: 0.02rem solid #ddd;
+}
+
+.hot-item {
+  width: 33.3333333333333333333333333333333%;
+  text-align: center;
+  height: 0.9rem;
+  line-height: 0.9rem;
+  float: left;
+  border-bottom: 0.02rem solid #ddd;
+}
+</style>
